d11debe098390275df9ade5f830c8be0cf82e0c8
Bogdan Pshonyak <bpshonyak@live.com>
2017-06-15 01:34:51 -0700

    page transitions working

---
+++ logic.js
@@ -0,0 +1,23 @@
+const links = document.querySelector('.links');
+const pageContainer = document.querySelector('.page-container');
+
+links.addEventListener('click', (event) => {
+    event.preventDefault();
+    const pageNumber = parseInt(event.target.dataset.pageNum);
+    let translateValue = 0;
+
+    switch (pageNumber) {
+        case 1:
+            translateValue = -100;
+            break;
+        case 2:
+            translateValue = -200;
+            break;
+        case 3:
+            translateValue = -300;
+            break;
+        default:
+            break;
+    }
+    pageContainer.setAttribute('style', `transform: translateY(${translateValue}%)`);
+});
