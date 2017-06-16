67b4b5a3cd77b028d4a74f6f619f2b48a7fc4065
Bogdan Pshonyak <bpshonyak@live.com>
2017-06-15 23:25:41 -0700

    swiper prototype

--- logic.js
+++ logic.js
@@ -21,3 +21,12 @@ links.addEventListener('click', (event) => {
     }
     pageContainer.setAttribute('style', `transform: translateY(${translateValue}%)`);
 });
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 0
+});
