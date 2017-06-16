ac4e0ded4eef217f7599c63c86424c20eeca22bb
Bogdan Pshonyak <bpshonyak@live.com>
2017-06-16 13:47:05 -0700

    var -> const

--- logic.js
+++ logic.js
@@ -22,7 +22,7 @@ links.addEventListener('click', (event) => {
     pageContainer.setAttribute('style', `transform: translateY(${translateValue}%)`);
 });
 
-var swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
